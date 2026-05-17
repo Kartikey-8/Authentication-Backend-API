4f69bf81cba8472ebe039cdcee13df1774e73d94
Kartikey <kartikeyyadav2622000@@gmail.com>
2026-05-17 10:33:08 +0530

    Initial backend setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(PORT , () => {
+  console.log(`Server running on port ${PORT}`);
+});
